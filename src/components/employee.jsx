89eb2e02ebe44d01b3f3412d64cebc3242c458cf
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-08 21:59:10 +0930

    table added.

--- src/components/employee.jsx
+++ src/components/employee.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { getEmployees} from './employeesAPI/API';
  
 class Employee extends Component {
     state = { 
@@ -10,9 +11,25 @@ class Employee extends Component {
     };
 
     render() { 
+
         return (  
             <div>
-
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                            <th>DOB</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
