0f7250e2351e65f50a1f89453cb75d15526c8c62
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-08 21:40:39 +0930

    header file created.

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() { 
+        return ( 
+            <div className="container text-center mt-5">
+                <h1>Employee Directory</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Header;
