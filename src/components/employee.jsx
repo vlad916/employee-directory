4123b33cf4cc6535b8a0b58ad8a84a3d82c774d3
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-09 20:18:32 +0930

    search component created.

--- src/components/employee.jsx
+++ src/components/employee.jsx
@@ -1,41 +1,54 @@
 import React, { Component } from "react";
 import EmployeesTable from './employeesTable';
+import SearchEmployees from './search';
 import { getEmployees } from "./employeesAPI/API";
 import _ from 'lodash';
 
 class Employee extends Component {
   state = {
     employees: [],
-    sortColumn: { path: 'image', order: 'asc' }
+    // sortColumn: { path: 'image', order: 'asc' }
   };
 
   componentDidMount() {
     this.setState({ employees: getEmployees() });
   };
 
-  handleSort = (sortColumn) => {
-    this.setState ({ sortColumn });
+  handleUpdateSearch = (event) => {
+    
+
   };
+
+  // handleSort = (sortColumn) => {
+  //   this.setState ({ sortColumn });
+  // };
 
   render() {
 
     const { length: count } = this.state.employees;  
     
     // if ( count === 0 ) return <p>There are no Employees in the directory</p>
-    const { employees, sortColumn } = this.state;
+    const { employees, search } = this.state;
 
-    _.orderBy (
-        [sortColumn.path],
-        [sortColumn.order]
-    );
+    // _.orderBy (
+    //     [sortColumn.path],
+    //     [sortColumn.order]
+    // );
 
     return (
       <div>
+        <div>
+          <SearchEmployees 
+          value={search}
+          onChange={this.handleUpdateSearch.bind(this)}
+          />
+        </div>
+        <br />
           <p>Showing { count } employees in the directory...</p>
        <EmployeesTable 
        employees={employees}
-       onSort={this.handleSort}
-       sortColumn={sortColumn}
+      //  onSort={this.handleSort}
+      //  sortColumn={sortColumn}
        />
       </div>
     );
---
+++ src/components/search.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const SearchEmployees = () => {
+    return ( 
+        <div>
+            <input type="text"/>
+        </div>
+     );
+}
+ 
+export default SearchEmployees;
