ac61b12556e693a7ff51fe23ce26eb5f1227ee5c
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-09 21:28:53 +0930

    tableHeader component created.

--- src/components/employeesTable.jsx
+++ src/components/employeesTable.jsx
@@ -1,61 +1,12 @@
 import React, { Component } from "react";
+import TableHeader from "./tableHeader";
 
 class EmployeesTable extends Component {
-  
-    raiseSort = (path) => {
-    const sortColumn = { ...this.props.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = (sortColumn.order === "asc") ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-    this.props.onSort(sortColumn);
-  };
-
   render() {
-    const { employees } = this.props;
+    const { employees, onSort, sortColumn } = this.props;
     return (
       <table className="table">
-        <thead>
-          <tr>
-            <th onClick={() => this.raiseSort("image")}>
-              Image
-              <button
-                className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
-                type="button"
-              ></button>
-            </th>
-            <th onClick={() => this.raiseSort("name")}>
-              Name
-              <button
-                className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
-                type="button"
-              ></button>
-            </th>
-            <th onClick={() => this.raiseSort("phone")}>
-              Phone
-              <button
-                className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
-                type="button"
-              ></button>
-            </th>
-            <th onClick={() => this.raiseSort("email")}>
-              Email
-              <button
-                className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
-                type="button"
-              ></button>
-            </th>
-            <th onClick={() => this.raiseSort("dob")}>
-              DOB
-              <button
-                className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
-                type="button"
-              ></button>
-            </th>
-          </tr>
-        </thead>
+        <TableHeader onSort={onSort} sortColumn={sortColumn} />
         <tbody>
           {employees.map((employee) => (
             <tr key={employee._id}>
---
+++ src/components/tableHeader.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+class TableHeader extends Component {
+  raiseSort = (path) => {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.path === path)
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    else {
+      sortColumn.path = path;
+      sortColumn.order = "asc";
+    }
+    this.props.onSort(sortColumn);
+  };
+
+  render() {
+    return (
+      <thead>
+        <tr>
+          <th onClick={() => this.raiseSort("image")}>
+            Image
+            <button
+              className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
+              type="button"
+            ></button>
+          </th>
+          <th onClick={() => this.raiseSort("name")}>
+            Name
+            <button
+              className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
+              type="button"
+            ></button>
+          </th>
+          <th onClick={() => this.raiseSort("phone")}>
+            Phone
+            <button
+              className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
+              type="button"
+            ></button>
+          </th>
+          <th onClick={() => this.raiseSort("email")}>
+            Email
+            <button
+              className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
+              type="button"
+            ></button>
+          </th>
+          <th onClick={() => this.raiseSort("dob")}>
+            DOB
+            <button
+              className="btn btn-light dropdown-toggle btn-sm m-2 text-danger"
+              type="button"
+            ></button>
+          </th>
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export default TableHeader;
